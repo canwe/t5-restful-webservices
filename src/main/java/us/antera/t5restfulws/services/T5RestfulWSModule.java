d8d0c49d9392ec3d556184b509c262ee24ecd1f4
bill.holloway <bill.holloway@71076e7a-cff4-11dd-bfe9-7982af55821d>
2008-12-22 06:57:02 +0000

    Initial import

---
+++ src/main/java/us/antera/t5restfulws/services/T5RestfulWSModule.java
@@ -0,0 +1,29 @@
+package us.antera.t5restfulws.services;
+
+import org.apache.tapestry5.ioc.OrderedConfiguration;
+import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.services.RequestFilter;
+import us.antera.t5restfulws.services.impl.RestfulWSDispatcher;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: holloway
+ * Date: Dec 21, 2008
+ * Time: 5:34:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class T5RestfulWSModule
+{
+    public RequestFilter buildRestfulWSDispatcher (Map<String, RestfulWS> services)
+    {
+        return new RestfulWSDispatcher (services);
+    }
+
+    public void contributeRequestHandler (OrderedConfiguration<RequestFilter> configuration,
+            @Local RequestFilter dispatcher)
+    {
+        configuration.add ("RestfulWebServices", dispatcher);
+    }
+}
