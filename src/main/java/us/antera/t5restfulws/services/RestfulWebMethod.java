bee2479a1b51d821da1490a55b66da8f0042ced4
bill.holloway <bill.holloway@71076e7a-cff4-11dd-bfe9-7982af55821d>
2008-12-25 01:57:11 +0000

    deleting idea files

---
+++ src/main/java/us/antera/t5restfulws/services/RestfulWebMethod.java
@@ -0,0 +1,17 @@
+package us.antera.t5restfulws.services;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: holloway
+ * Date: Dec 24, 2008
+ * Time: 12:04:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Target(ElementType.METHOD)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Documented
+public @interface RestfulWebMethod
+{
+}
